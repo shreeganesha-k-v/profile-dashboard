679b700af232feb22fe8c3c733e342d884f1ab8d
Shreeganesha <shreeganeshakv444@gmail.com>
2026-03-01 16:46:20 +0530

    initial code commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home';
+import { SkillsComponent } from './components/skills/skills';
+import { AboutComponent } from './components/about/about';
+import { ProjectsComponent } from './components/projects/projects';
+import { ExperienceComponent } from './components/experience/experience';
+import { CertificationsComponent } from './components/certifications/certifications';
+import { ContactComponent } from './components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ { path: '', component: HomeComponent },
+ { path: 'about', component: AboutComponent },
+ { path: 'skills', component: SkillsComponent },
+ { path: 'projects', component: ProjectsComponent },
+ { path: 'experience', component: ExperienceComponent },
+ { path: 'certifications', component: CertificationsComponent },
+ { path: 'contact', component: ContactComponent }
+];
---
+++ src/app/components/skills/skills.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  imports: [],
+  templateUrl: './skills.html',
+  styleUrl: './skills.css',
+})
+export class SkillsComponent {
+  skills = [
+ "Java",
+ "Spring Boot",
+ "Microservices",
+ "Angular",
+ "JavaScript",
+ "Oracle",
+ "Docker",
+ "REST APIs"
+];
+}
