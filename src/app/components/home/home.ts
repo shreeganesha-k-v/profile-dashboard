5dddbf7156b3121262afd17b199eb9c70a4ec3cf
Shreeganesha <shreeganeshakv444@gmail.com>
2026-03-12 10:42:49 +0530

    Fix: Improve UX

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
