3c70931d06e5681208dd25921d093cc722f5033a
Shreeganesha <shreeganeshakv444@gmail.com>
2026-03-13 10:40:52 +0530

    Fix : Contact page updated

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.html',
   styleUrls: ['./contact.css'],
 })
@@ -10,10 +11,19 @@ export class ContactComponent {
   phone = "+91 9945154957";
   linkedIn = "https://www.linkedin.com/in/shreeganesha-k-v-3906121b3/";
   github = "https://github.com/shreeganesha-k-v";
+  copied = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
 
   copyPhone() {
-    navigator.clipboard.writeText(this.phone);
-    alert("Phone number copied!");
+    navigator.clipboard.writeText(this.phone).then(() => {
+      this.copied = true;
+      this.cdr.detectChanges();
+      setTimeout(() => {
+        this.copied = false;
+        this.cdr.detectChanges();
+      }, 2000);
+    });
   }
 
   viewResume() {
