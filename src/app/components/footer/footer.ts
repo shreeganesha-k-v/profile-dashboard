1e3d4434502b16f1620a2aa20aaa8931a30adf60
Shreeganesha <shreeganeshakv444@gmail.com>
2026-04-07 11:00:26 +0530

    Fix : Fixing footer component to improve UX

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './footer.html',
   styleUrls: ['./footer.css'],
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+
+  socialLinks = [
+    { name: 'GitHub', url: 'https://github.com/shreeganesha-k-v', icon: 'bi bi-github' },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/shreeganesha-k-v-3906121b3/', icon: 'bi bi-linkedin' },
+    { name: 'LeetCode', url: 'https://leetcode.com/u/Shreeganesha_K_V/', icon: 'bi bi-code-slash' }
+  ];
+
+  quickLinks = [
+    { name: 'Home', route: '/' },
+    { name: 'About', route: '/about' },
+    { name: 'Projects', route: '/projects' },
+    { name: 'Contact', route: '/contact' }
+  ];
 
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
